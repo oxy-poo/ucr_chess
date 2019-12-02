cf15f3e4fee2432708c0690d54b31e906434492b
Server <56360050+NickSantini32@users.noreply.github.com>
2019-12-01 21:04:21 -0800

    update 2 (multiple urls)

--- UCRchess/sample/views.py
+++ UCRchess/sample/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def homePage(request):
     return render(request, 'sample/index.html' )#HttpResponse("Hello!")
+def about(request):
+    return render(request, 'sample/about_page.html' )#HttpResponse("Hello!")
